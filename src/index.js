ca7a20c417f4daf2e64ff6a91111c9f898161241
Hanna Zalatarevich <a.zoloto@mail.ru>
2022-08-05 22:06:08 +0300

    init: start task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+     if (n<0) {
+        n=0-n;
+     }
+ 
+        let word=String(n).split('').reverse().join('');
+        
+        console.log(Number(word));
+        return Number(word);
+       
+      }
+
